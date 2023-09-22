5a654ebb9e68c4b7232953923cd7566a0b6d9b16
Abraham Lipa Calabilla <alipacala@gmail.com>
2023-09-22 11:44:45 -0500

    cambios

--- inc/bootstrap.php
+++ inc/bootstrap.php
@@ -1,7 +1,7 @@
 <?php
-define("PROJECT_ROOT_PATH", __DIR__ . "/../");
+define("PROJECT_ROOT_PATH", __DIR__ . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR);
 
-require_once PROJECT_ROOT_PATH . "/inc/config.php";
-require_once PROJECT_ROOT_PATH . "/inc/database.php";
-require_once PROJECT_ROOT_PATH . "/inc/error_handlers.php";
+require_once realpath(PROJECT_ROOT_PATH . DIRECTORY_SEPARATOR . 'inc' . DIRECTORY_SEPARATOR . 'config.php');
+require_once realpath(PROJECT_ROOT_PATH . DIRECTORY_SEPARATOR . 'inc' . DIRECTORY_SEPARATOR . 'database.php');
+require_once realpath(PROJECT_ROOT_PATH . DIRECTORY_SEPARATOR . 'inc' . DIRECTORY_SEPARATOR . 'error_handlers.php');
 ?>
