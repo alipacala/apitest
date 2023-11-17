44f51a323a93f884b3107a792cef79acf5a3720c
Abraham Lipa Calabilla <alipacala@gmail.com>
2023-11-17 14:13:58 -0500

    cambios

--- models/RoomingDb.php
+++ models/RoomingDb.php
@@ -53,57 +53,88 @@ class RoomingDb extends Database
   public function listarRoomingConDatos($fecha)
   {
     $query = "SELECT
-    COALESCE(p.nombre_producto, '') AS nombre_producto,
-    COALESCE(p.descripcion_del_producto, '') AS abreviatura_producto,
-    h.nro_habitacion AS nro_habitacion,
-    COALESCE(ch.id_checkin, '') AS id_checkin,
-    COALESCE(ch.nro_registro_maestro, '') AS nro_registro_maestro,
-    COALESCE(ch.nro_reserva, re.nro_reserva) AS nro_reserva,
-    COALESCE(ch.nombre, re.nombre) AS nombre,
-    COALESCE(ch.nro_personas, re.nro_personas) AS nro_personas,
-    COALESCE(fechas_minmax.fecha_in, rh.fecha_ingreso) AS fecha_in,
-    re.estado_pago AS estado_pago,
-    COALESCE(r.estado, 'NA') AS estado,
-    COALESCE(CASE
-      WHEN r.cambiado IS NULL THEN DATE_ADD(fechas_minmax.fecha_out, INTERVAL 1 DAY)
-        ELSE fechas_minmax.fecha_out
-      END, rh.fecha_salida) AS fecha_out,
-
-    CASE WHEN ch.id_checkin IS NOT NULL THEN 1 ELSE 0 END AS ocupado,
-    CASE WHEN re.nro_reserva IS NOT NULL THEN 1 ELSE 0 END AS reservado,
-    CASE WHEN COALESCE(DATE_ADD(fechas_minmax.fecha_out, INTERVAL 1 DAY), rh.fecha_salida) = :fecha5 THEN 1 ELSE 0 END AS de_salida
-
-    FROM habitaciones h
-    LEFT JOIN rooming r ON r.nro_habitacion = h.nro_habitacion AND (:fecha1 = r.fecha OR (
-      r.cambiado IS NULL AND :fecha2 = DATE_ADD(r.fecha, INTERVAL 1 DAY)
-      )
-    )
-
-
-    LEFT JOIN reservahabitaciones rh ON rh.nro_habitacion = h.nro_habitacion AND rh.fecha_ingreso <= :fecha3 AND rh.fecha_salida >= :fecha4
-    LEFT JOIN reservas re ON re.nro_reserva = rh.nro_reserva
-
-    LEFT JOIN cheking ch ON ch.nro_reserva = re.nro_reserva
-    LEFT JOIN productos p ON p.id_producto = h.id_producto
-
-    LEFT JOIN (
-        SELECT ro.nro_registro_maestro, ro.nro_habitacion, MIN(ro.fecha) AS fecha_in, MAX(ro.fecha) AS fecha_out
-        FROM rooming ro
-        INNER JOIN cheking ch ON ch.id_checkin = ro.id_checkin
-        INNER JOIN reservas re ON re.nro_reserva = ch.nro_reserva
-        GROUP BY nro_registro_maestro, nro_habitacion
-    ) AS fechas_minmax ON fechas_minmax.nro_registro_maestro = r.nro_registro_maestro AND fechas_minmax.nro_habitacion = r.nro_habitacion
-    
+      COALESCE(p.nombre_producto, '') AS nombre_producto,
+      COALESCE(p.descripcion_del_producto, '') AS abreviatura_producto,
+      h.nro_habitacion AS nro_habitacion,
+      '' AS id_checkin,
+      '' AS nro_registro_maestro,
+      COALESCE(re.nro_reserva, '') AS nro_reserva,
+      COALESCE(re.nombre, '') AS nombre,
+      COALESCE(rh.nro_personas, '') AS nro_personas,
+      COALESCE(rh.fecha_ingreso, '') AS fecha_in,
+      COALESCE(re.estado_pago, '') AS estado_pago,
+      '' AS estado,
+      COALESCE(rh.fecha_salida, '') AS fecha_out,
+  
+      CASE WHEN re.nro_registro_maestro IS NOT NULL THEN 1 ELSE 0 END AS ocupado,
+      CASE WHEN re.nro_reserva IS NOT NULL THEN 1 ELSE 0 END AS reservado,
+      CASE WHEN re.nro_registro_maestro IS NULL AND re.nro_reserva IS NULL THEN 1 ELSE 0 END AS libre,
+      CASE WHEN re.nro_registro_maestro IS NOT NULL AND re.nro_reserva IS NOT NULL THEN 1 ELSE 0 END AS reservado_pero_ocupado,
+      CASE WHEN rh.fecha_salida = :fecha1 THEN 1 ELSE 0 END AS de_salida
+  
+      FROM habitaciones h
+  
+      LEFT JOIN reservahabitaciones rh ON rh.nro_habitacion = h.nro_habitacion AND rh.fecha_ingreso <= :fecha2 AND rh.fecha_salida >= :fecha3
+      LEFT JOIN reservas re ON re.nro_reserva = rh.nro_reserva
+      LEFT JOIN productos p ON p.id_producto = h.id_producto
+  
     WHERE h.id_unidad_de_negocio = 3
-
-    GROUP BY h.nro_habitacion
-    ORDER BY h.nro_habitacion ASC";
+      
+      /*-----------------------------------------------------------------------------------------------------------------------------------------*/
+      UNION ALL
+      /*-----------------------------------------------------------------------------------------------------------------------------------------*/
+  
+    SELECT
+      COALESCE(p.nombre_producto, '') AS nombre_producto,
+      COALESCE(p.descripcion_del_producto, '') AS abreviatura_producto,
+      h.nro_habitacion AS nro_habitacion,
+      COALESCE(ch.id_checkin, '') AS id_checkin,
+      COALESCE(ch.nro_registro_maestro, '') AS nro_registro_maestro,
+      COALESCE(ch.nro_reserva, '') AS nro_reserva,
+      COALESCE(ch.nombre, '') AS nombre,
+      COALESCE(ch.nro_personas, '') AS nro_personas,
+      COALESCE(fechas_minmax.fecha_in, '') AS fecha_in,
+      '' AS estado_pago,
+      COALESCE(r.estado, '') AS estado,
+      COALESCE(CASE
+        WHEN r.cambiado IS NULL THEN DATE_ADD(fechas_minmax.fecha_out, INTERVAL 1 DAY)
+          ELSE fechas_minmax.fecha_out
+        END, '') AS fecha_out,
+  
+      CASE WHEN ch.id_checkin IS NOT NULL THEN 1 ELSE 0 END AS ocupado,
+      0 AS reservado,
+      CASE WHEN ch.id_checkin IS NULL THEN 1 ELSE 0 END AS libre,
+      0 AS reservado_pero_ocupado,
+      CASE WHEN DATE_ADD(fechas_minmax.fecha_out, INTERVAL 1 DAY) = :fecha4 THEN 1 ELSE 0 END AS de_salida
+  
+      FROM habitaciones h
+      LEFT JOIN rooming r ON r.nro_habitacion = h.nro_habitacion AND (:fecha5 = r.fecha OR (
+        r.cambiado IS NULL AND :fecha6 = DATE_ADD(r.fecha, INTERVAL 1 DAY)
+        )
+      )
+      
+      
+      LEFT JOIN (
+          SELECT ro.nro_registro_maestro, ro.nro_habitacion, MIN(ro.fecha) AS fecha_in, MAX(ro.fecha) AS fecha_out
+          FROM rooming ro
+          INNER JOIN cheking ch ON ch.id_checkin = ro.id_checkin
+          GROUP BY nro_registro_maestro, nro_habitacion
+      ) AS fechas_minmax ON fechas_minmax.nro_registro_maestro = r.nro_registro_maestro AND fechas_minmax.nro_habitacion = r.nro_habitacion
+  
+      LEFT JOIN cheking ch ON ch.nro_registro_maestro = fechas_minmax.nro_registro_maestro
+      LEFT JOIN productos p ON p.id_producto = h.id_producto
+  
+      
+      WHERE h.id_unidad_de_negocio = 3
+  
+      ORDER BY nro_habitacion ASC;";
     $params = array(
       ["nombre" => "fecha1", "valor" => $fecha, "tipo" => PDO::PARAM_STR],
       ["nombre" => "fecha2", "valor" => $fecha, "tipo" => PDO::PARAM_STR],
       ["nombre" => "fecha3", "valor" => $fecha, "tipo" => PDO::PARAM_STR],
       ["nombre" => "fecha4", "valor" => $fecha, "tipo" => PDO::PARAM_STR],
-      ["nombre" => "fecha5", "valor" => $fecha, "tipo" => PDO::PARAM_STR]
+      ["nombre" => "fecha5", "valor" => $fecha, "tipo" => PDO::PARAM_STR],
+      ["nombre" => "fecha6", "valor" => $fecha, "tipo" => PDO::PARAM_STR]
     );
 
     return $this->executeQuery($query, $params);
