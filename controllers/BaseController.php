6090e3b6f17bb29e5dde8c8979830eff0c2f99a8
Abraham Lipa Calabilla <alipacala@gmail.com>
2023-12-13 10:26:35 -0500

    cambios

--- controllers/BaseController.php
+++ controllers/BaseController.php
@@ -210,9 +210,9 @@ class BaseController
   {
     return [
       "mensaje" => $e->getMessage(),
-      "archivo" => $e->getPrevious()->getFile() ?? $e->getFile(),
-      "linea" => $e->getPrevious()->getLine() ?? $e->getLine(),
-      "trace" => $e->getPrevious()->getTrace() ?? $e->getTrace()
+      "archivo" => $e->getPrevious()?->getFile() ?? $e->getFile(),
+      "linea" => $e->getPrevious()?->getLine() ?? $e->getLine(),
+      "trace" => $e->getPrevious()?->getTrace() ?? $e->getTrace()
     ];
   }
 }
